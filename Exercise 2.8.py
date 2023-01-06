14caa46969633354244e7b2a4d90fb90511fbafb
aluhmann <apl323@lehigh.edu>
2023-01-06 07:18:43 -0600

    Exercise 2.8.py

---
+++ Exercise 2.8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exercise 2.8 Dietel
+
+Created on Thu Jan  5 23:21:37 2023
+
+@author: andrewluhmann3
+"""
+print('number\t square\t cube')
+print(0, '\t', '\t', 0 ** 2, '\t', '\t', 0 ** 3)
+print(1, '\t', '\t', 1 ** 2, '\t', '\t', 1 ** 3)
+print(2, '\t', '\t', 2 ** 2, '\t', '\t', 2 ** 3)
+print(3, '\t', '\t', 3 ** 2, '\t', '\t', 3 ** 3)
+print(4, '\t', '\t', 4 ** 2, '\t', '\t', 4 ** 3)
+print(5, '\t', '\t', 5 ** 2, '\t', '\t', 5 ** 3)
