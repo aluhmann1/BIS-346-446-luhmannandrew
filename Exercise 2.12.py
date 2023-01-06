a4d283b828e4162b3f9f221d983f3494d88ad891
aluhmann <apl323@lehigh.edu>
2023-01-06 07:22:05 -0600

    Exercise 2.12.py

---
+++ Exercise 2.12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Exercixe 2.12 Dietel
+
+Created on Fri Jan  6 07:19:22 2023
+
+@author: andrewluhmann3
+"""
+
+print('Amount after 10 years:', 1000 * (1 + .07) ** 10)
+
+print('Amount after 20 years:', 1000 * (1 + .07) ** 20)
+
+print('Amount after 30 years:', 1000 * (1 + .07) ** 30)
