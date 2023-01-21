78054a61e8d87e4e20820f87e58f0b2f76c6bf87
aluhmann <apl323@lehigh.edu>
2023-01-21 12:03:31 -0600

    Misc

---
+++ Exercise7.1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 21:50:07 2023
+
+@author: andrewluhmann3
+"""
+import numpy as np
+np.array([range(i, i + 1) for i in [1, 2, 3]])
+//edit
